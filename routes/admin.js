56da7a158259d15a7c4d3591920915883f0e5694
Jorge Flores <flores.jorgeluis@gmail.com>
2021-07-21 18:54:53 -0500

    tercer cambio

--- routes/admin.js
+++ routes/admin.js
@@ -5,12 +5,7 @@ app.get("/", (req,res)=>{
 //res.send("Bienvenido a Admin")
 
 var items = [
-    {name:'node.js',url:'https://nodejs.org/en/'},
-    {name:'ejs',url:'https://ejs.co'},
-    {name:'expressjs',url:'https://expressjs.com'},
-    {name:'vuejs',url:'https://vuejs.org'},
-    {name:'nextjs',url:'https://nextjs.org'},
-    {name:'Amazon',url:'https://amazon.com'}
+    {name:'node.js',url:'https://nodejs.org/en/'}
 ];
 
 var isAdmin = true;
