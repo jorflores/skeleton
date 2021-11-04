4b8e305e57e89dc9fed599fd21806d65066f2cf3
Jorge Flores <flores.jorgeluis@gmail.com>
2021-11-04 09:22:31 -0600

    starter skeleton

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 // # Dependencias
-var config = require("./config");
 var express = require("express");
 var morgan = require("morgan");
+var mongoose = require('mongoose');
+var dotenv = require("dotenv");
 
-var index = require("./routes/index");
-var admin = require("./routes/admin");
-var api = require("./routes/api");
+dotenv.config()
 
 var app = express();
-app.use(morgan('dev'));
 
+// Connection to MongoDB
+
+
+
+// Middlewares
+app.use(morgan('dev'));
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.use('/', index);
-app.use('/admin', admin)
-app.use('/api', api)
+// importing routes
+const indexRoutes = require('./routes/indexroutes');
+
+// Routes
+app.use('/', indexRoutes);
 
 
-app.listen(config.app.port,() =>{
-   console.log(`Server running on port  ${config.app.port}`) 
+app.listen(process.env.PORT,() =>{
+   console.log(`Server running on port  ${process.env.PORT}`) 
 } )
 
 
--- routes/api.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.get("/characters", (req,res)=>{
-    res.send("Bienvenido a characters")
-})
-
-module.exports = app;
